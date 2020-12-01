9772a344218297b7ef59f8178cf9ebfdca1231fe
bsolla <bsolla@ucm.es>
2020-12-01 01:30:27 +0100

    Cambios en android game

--- p1/androidgame/src/main/java/ucm/vm/androidgame/MainActivity.java
+++ p1/androidgame/src/main/java/ucm/vm/androidgame/MainActivity.java
@@ -4,12 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ucm.vm.engine.android.MySurfaceView;
+
 public class MainActivity extends AppCompatActivity {
+    MySurfaceView _renderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Preparamos el contenido de la actividad.
+        _renderView = new MySurfaceView(this);
+        setContentView(_renderView);
     }
 
+    @Override
+    protected void onResume() {
+        // Avisamos a la vista (que es la encargada del active render)
+        // de lo que está pasando.
+        super.onResume();
+        _renderView.resume();
+
+    } // onResume
+
+    @Override
+    protected void onPause() {
+        // Avisamos a la vista (que es la encargada del active render)
+        // de lo que está pasando.
+        super.onPause();
+        _renderView.pause();
+
+    } // onPause
 }
